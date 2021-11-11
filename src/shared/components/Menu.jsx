0df743d35b46e42f41acacf6acb8287de3135e42
Nate Kliegl <nkliegl2@gmail.com>
2021-11-11 09:31:30 -0600

    adds logout button and header

--- src/shared/components/Menu.jsx
+++ src/shared/components/Menu.jsx
@@ -3,11 +3,9 @@ import { NavLink } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
+
 import Button from "@mui/material/Button";
-import Tab from "@mui/material/Tab";
-import Tabs from "@mui/material/Tabs";
+
 import "./Menu.css";
 
 export default function Menu() {
@@ -18,24 +16,20 @@ export default function Menu() {
       <nav>
         <AppBar position="static" sx={{ bgcolor: "#1f2f53" }}>
           <Toolbar variant="regular">
-            {/* <Typography color="white"> */}
-            {/* <Button sx={{ bgcolor: "#fffb00" }}> */}
+            <header className="menuHeader">Migos 5 Card Draw</header>
             <NavLink to="/about" className="link">
               About
             </NavLink>
-            {/* </Button> */}
+
             {!user.username && (
               <>
-                {/* <Button sx={{ bgcolor: "#fffb00" }}> */}
                 <NavLink to="/login" className="link">
                   Login
                 </NavLink>
-                {/* </Button> */}
-                {/* <Button sx={{ bgcolor: "#fffb00" }}> */}
+
                 <NavLink to="/signup" className="link">
                   Signup
                 </NavLink>
-                {/* </Button> */}
               </>
             )}
             {user.username && (
@@ -47,10 +41,11 @@ export default function Menu() {
                   Game
                 </NavLink>
 
-                <Button onClick={clearState}>Logout</Button>
+                <button onClick={clearState} className="logout">
+                  Logout
+                </button>
               </>
             )}
-            {/* </Typography> */}
           </Toolbar>
         </AppBar>
       </nav>
