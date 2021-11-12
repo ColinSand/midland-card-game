422be55b4d33dcb52329ded52b60f99594e98700
Colin Sand <Ottermij2@gmail.com>
2021-11-12 10:34:47 -0600

    Creates a new deck from suits and card values

--- src/shared/context/GameContext.jsx
+++ src/shared/context/GameContext.jsx
@@ -7,8 +7,63 @@
 // each player will technically be informed of who all got what cards, those cards will be hidden (face down) unless
 // you are the player who recieved those cards, those will be face up so you can see them
 
+// need to know whow the players are , what their cards are
+// how many cards they get, how to swap cards
+// sockets are going to trigger this functionaility
+// need to know which player needs what
+// staying will emit (sockets) for myself
+
+// in a lot of ways these functions will sort of b
+
+// need to get all of the functionality on the user side:
+//     - Join Room
+//     - Deal Card
+//     - Fold
+//     - Stay
+//     - Start Game
+//     - Game Over
+//     - Leave Room
+//     - Close Room
+
 import React from "react";
 
 export default function GameContext(props) {
+  const [deck, setDeck] = useState([]);
+  // the cardsDealt array should be an array of objects, have the usernames of each player, then the individual cards
+  const [cardsDealt, setCardsDealt] = useState([]);
+  const [players, setPlayers] = useState([]);
+  const [isHost, setIsHost] = useState(false);
+  const [isActive, setIsActice] = useState(false);
+  const [isTurn, setIsTurn] = useState(false);
+
+  let cardSuits = ["diamonds", "spades", "hearts", "clubs"];
+  let cardValues = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A",
+  ];
+
+  function createDeck() {
+    let newDeck = new Array();
+    for (let i = 0; i < cardSuits.length; i++) {
+      for (let x = 0; x < cardValues.length; x++) {
+        let card = { Value: cardSuits[x], Suit: cardSuits[i] };
+        newDeck.push(card);
+      }
+    }
+    setDeck(newDeck);
+    return newDeck;
+  }
+
   return <GameContext.Provider></GameContext.Provider>;
 }
