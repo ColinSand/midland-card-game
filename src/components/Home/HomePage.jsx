ceeda236efe8dc339a642b1449e97d0ffb0077d6
Seth Allgire <seth.allgire@gmail.com>
2021-11-16 10:09:15 -0600

    updates game context

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-=======
 import React, { useContext, useState } from "react";
->>>>>>> 5e1576058668a6b3dfa2dd2c3f5a335a7d839cfa
 import { useNavigate } from "react-router-dom";
 import {
   Avatar,
@@ -17,11 +13,7 @@ import {
   Alert,
 } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-<<<<<<< HEAD
-
-=======
 import { UserContext } from "../../shared/context/UserContext";
->>>>>>> 5e1576058668a6b3dfa2dd2c3f5a335a7d839cfa
 import { GroupAddOutlined, GroupsOutlined } from "@mui/icons-material";
 
 const theme = createTheme();
@@ -30,10 +22,7 @@ export default function HomePage() {
   const [gameId, setGameId] = useState("");
   const [error, setError] = useState(null);
   const navigate = useNavigate();
-<<<<<<< HEAD
-=======
   const [hosting, isHost, setIsHost] = useContext(UserContext);
->>>>>>> 5e1576058668a6b3dfa2dd2c3f5a335a7d839cfa
 
   const generateGameId = () => {
     return Math.random().toString(36).substr(2, 6);
@@ -158,10 +147,7 @@ export default function HomePage() {
                   onClick={(e) => {
                     const newGameId = generateGameId();
                     navigate(`/game/${newGameId}`);
-<<<<<<< HEAD
-=======
                     hosting();
->>>>>>> 5e1576058668a6b3dfa2dd2c3f5a335a7d839cfa
                   }}
                 >
                   Create Table
