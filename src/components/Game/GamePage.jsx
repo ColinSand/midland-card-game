c793399d39dee1e9dc8359cd9bb9e83536114f61
Colin Sand <Ottermij2@gmail.com>
2021-11-16 20:15:27 -0600

    Adds the game code to the page

--- src/components/Game/GamePage.jsx
+++ src/components/Game/GamePage.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { useContext } from "react";
 import { GameContext } from "../../shared/context/GameContext";
 import Button from "@mui/material/Button";
+import { UserContext } from "../../shared/context/UserContext";
 
 // here we go
 // nate's chat will be shown on the left side, towards the bottom
+
 // need to show the game number, hosted by ${host} with a deal/start button that only the host can see.
 
 // top right will have the name of the player who's turn it currently is
@@ -13,23 +15,40 @@ import Button from "@mui/material/Button";
 // your cards face up with the draw and stay buttons, then check up to 3 boxes to select cards to draw/replace.
 // most of the card functionality will come from seth
 
+//further breakdown:
+// display game code-done
+// show host's name
+// card functionality?
+// choose which cards to discard on your turn?
+// set up a spot for chat to be implemented
+// show whos turn it currently is
+//
+
 function GamePage() {
+  const { isHost } = useContext(UserContext);
+
   const {
     deck,
     cardsDealt,
     players,
     isActive,
-    isHost,
+
     isTurn,
     createDeck,
     shuffleDeck,
     dealCards,
   } = useContext(GameContext);
-
+  console.log(window.location.href);
   return (
     <>
       <div>
         <br></br>
+        <div>
+          Join code for this game:
+          {window.location.href.slice(-6)}
+        </div>
+        {/* the following line is having issues with the isHost, that may not be done correctly in the userContext */}
+        {/* <div>Hosted By:{isHost}</div> */}
         <Button
           variant="contained"
           onClick={() => {
