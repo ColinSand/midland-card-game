7383f80392e51a6150993d038b8b281e7bd34bd4
Colin Sand <Ottermij2@gmail.com>
2021-11-16 19:19:34 -0600

    Initial commit-planning

--- src/components/Game/GamePage.jsx
+++ src/components/Game/GamePage.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import { useContext } from "react";
 import { GameContext } from "../../shared/context/GameContext";
+import Button from "@mui/material/Button";
+
+// here we go
+// nate's chat will be shown on the left side, towards the bottom
+// need to show the game number, hosted by ${host} with a deal/start button that only the host can see.
+
+// top right will have the name of the player who's turn it currently is
+
+// the center of this page should have all of the usernames with their cards (face down)
+// your cards face up with the draw and stay buttons, then check up to 3 boxes to select cards to draw/replace.
+// most of the card functionality will come from seth
 
 function GamePage() {
   const {
@@ -18,18 +29,15 @@ function GamePage() {
   return (
     <>
       <div>
-        <button
+        <br></br>
+        <Button
+          variant="contained"
           onClick={() => {
             createDeck();
           }}
         >
-          Shuffle Deck
-        </button>
-        <button
-          onClick={() => {
-            dealCards();
-          }}
-        ></button>
+          Shuffle Deck, Start Game
+        </Button>
       </div>
     </>
   );
