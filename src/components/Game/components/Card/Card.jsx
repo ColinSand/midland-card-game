122f5c3bc9f853a86fcdd8c4a2b28a399628a3e7
Seth Allgire <seth.allgire@gmail.com>
2021-11-18 16:14:09 -0600

    adds card styling

--- src/components/Game/components/Card/Card.jsx
+++ src/components/Game/components/Card/Card.jsx
@@ -5,14 +5,15 @@ const Card = ({ face, suit, showCard }) => {
   return (
     <>
       {showCard && (
-        <div className="card-container">
-          <div className="card-flex">{suit}</div>
-          <div className="card-flex card-value">{face}</div>
-          <div className="card-flex bottom-right">{suit}</div>
+        <div className="card-surround">
+          <div className="top-left">{suit}</div>
+          <br />
+          <div className="card-value">{face}</div>
+          <div className="bottom-right">{suit}</div>
         </div>
       )}
       {!showCard && (
-        <div className="card-container">
+        <div className="card-surround">
           <div className="card-back"></div>
         </div>
       )}
--- src/shared/context/GameContext.jsx
+++ src/shared/context/GameContext.jsx
@@ -29,7 +29,7 @@ export function GameProvider(props) {
   // and a 'hand'(consisting of an array of 5 cards defined upon 'startGameDeal' function and updated
   // when they trigger 'draw' function)
 
-  let cardSuits = ["diamonds", "spades", "hearts", "clubs"];
+  let cardSuits = ["d", "s", "h", "c"];
   let cardValues = [
     "2",
     "3",
